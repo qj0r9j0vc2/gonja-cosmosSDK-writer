041fe17e48f5d17352c8d16836c90a0e27588423
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2024-03-13 23:10:27 +0900

    add

---
+++ go.mod
@@ -0,0 +1,35 @@
+module main.go
+
+go 1.21.6
+
+require (
+	github.com/genelet/determined v1.10.2
+	github.com/hashicorp/hcl/v2 v2.20.0
+	github.com/zclconf/go-cty v1.14.3
+)
+
+require (
+	github.com/agext/levenshtein v1.2.2 // indirect
+	github.com/apparentlymart/go-cidr v1.1.0 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/bmatcuk/doublestar v1.3.4 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/goph/emperror v0.17.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/noirbizarre/gonja v0.0.0-20200629003239-4d051fd0be61 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/sirupsen/logrus v1.3.0 // indirect
+	github.com/zclconf/go-cty-yaml v1.0.3 // indirect
+	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"github.com/noirbizarre/gonja"
+	"gopkg.in/yaml.v3"
+	"os"
+	"reflect"
+)
+
+var (
+	configTomlJ2 = gonja.Must(gonja.FromFile("templates/config.toml.j2"))
+	appTomlJ2    = gonja.Must(gonja.FromFile("templates/app.toml.j2"))
+)
+
+type CustomConfig struct {
+	// ConfigToml specifies parameter(s) in config.toml
+	ConfigToml []Entry `yaml:"configToml"`
+	// ConfigToml specifies parameter(s) in config.toml
+	AppToml []Entry `yaml:"appToml"`
+}
+
+type Entry interface{}
+
+func ConvertToMapStringInterface(entryList []Entry) map[string]interface{} {
+	mapInterface := make(map[string]interface{})
+	for _, entry := range entryList {
+		mapInterface = mergeMaps(mapInterface, interfaceConverter(entry))
+	}
+	return mapInterface
+}
+
+func mergeMaps(maps ...map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for _, m := range maps {
+		for key, value := range m {
+			result[key] = value
+		}
+	}
+	return result
+}
+
+func interfaceConverter(entry interface{}) map[string]interface{} {
+	mapInterface := make(map[string]interface{})
+	var convertValue interface{}
+
+	convert, ok := entry.(map[string]interface{})
+	if ok {
+		for key, value := range convert {
+			if convertValue, ok = value.(string); ok {
+				mapInterface[key] = convertValue
+			} else if convertValue, ok = value.(int); ok {
+				mapInterface[key] = convertValue
+			} else if convertValue, ok = value.(bool); ok {
+				mapInterface[key] = convertValue
+			} else {
+				mapInterface[key] = interfaceConverter(value)
+			}
+		}
+	} else if reflect.TypeOf(entry).Kind() == reflect.Slice {
+		s := reflect.ValueOf(entry)
+		for i := 0; i < s.Len(); i++ {
+			mapInterface = mergeMaps(mapInterface, interfaceConverter(s.Index(i).Interface()))
+		}
+	} else {
+		mapInterface = mergeMaps(mapInterface, interfaceConverter(entry))
+	}
+
+	return mapInterface
+}
+
+func parse() {
+	var config CustomConfig
+	f, err := os.ReadFile("config.yaml")
+
+	err = yaml.Unmarshal(f, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	//if len(config.ConfigToml) <= 0 {
+	//	return
+	//}
+	//fmt.Printf("======== custom config.toml ========\n")
+	//for key, value := range ConvertToMapStringInterface(config.ConfigToml) {
+	//	fmt.Printf("%s, %s\n", key, value)
+	//}
+	//
+	//configTomlOut, err := configTomlJ2.Execute(ConvertToMapStringInterface(config.ConfigToml))
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//configTomlOutFile, err := os.Create("out/config.toml")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//_, err = configTomlOutFile.Write([]byte(configTomlOut))
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	if len(config.AppToml) <= 0 {
+		return
+	}
+	fmt.Printf("======== custom app.toml ========\n")
+	for key, value := range ConvertToMapStringInterface(config.AppToml) {
+		fmt.Printf("%s, %s\n", key, value)
+	}
+
+	appTomlOut, err := appTomlJ2.Execute(ConvertToMapStringInterface(config.AppToml))
+	if err != nil {
+		panic(err)
+	}
+
+	appTomlOutFile, err := os.Create("out/app.toml")
+	if err != nil {
+		panic(err)
+	}
+	_, err = appTomlOutFile.Write([]byte(appTomlOut))
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func main() {
+	parse()
+}
